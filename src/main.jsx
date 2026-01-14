7b394e5a8a3cf1b7c96fe72505f1f8bfff6a82f3
0png <blackwolfyt0708@gmail.com>
2026-01-14 13:13:51 +0800

    fix: set body background color to match theme and prevent white flash

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// 隱藏 skeleton loader
-const hideSkeleton = () => {
-  const skeleton = document.getElementById('skeleton-loader')
-  if (skeleton) {
-    skeleton.classList.add('hidden')
-    // 動畫結束後移除 DOM
-    setTimeout(() => skeleton.remove(), 300)
-  }
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-// React 渲染後隱藏 skeleton
-requestAnimationFrame(() => {
-  requestAnimationFrame(hideSkeleton)
-})
